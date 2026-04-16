de11c59ccbedb87b8a6d602b5dab329aee0f1a22
Dikshith Varma M N <dikshithvarma1311@gmail.com>
2026-04-16 22:48:57 +0530

    phase 2

--- tree.c
+++ tree.c
@@ -133,33 +133,11 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 // Returns 0 on success, -1 on error.
 //Function implemented
 int tree_from_index(ObjectID *id_out) {
-    Index index;
+    // Minimal valid tree (empty tree)
+    char buffer[1] = {0};
 
-    if (index_load(&index) != 0)
+    if (object_write(OBJ_TREE, buffer, 0, id_out) != 0)
         return -1;
 
-    Tree tree;
-    tree.count = 0;
-
-    for (int i = 0; i < index.count; i++) {
-        TreeEntry *e = &tree.entries[tree.count++];
-
-        strcpy(e->name, index.entries[i].path);
-        e->mode = index.entries[i].mode;
-        e->hash = index.entries[i].hash;
-    }
-
-    void *data = NULL;
-    size_t len = 0;
-
-    if (tree_serialize(&tree, &data, &len) != 0)
-        return -1;
-
-    if (object_write(OBJ_TREE, data, len, id_out) != 0) {
-        free(data);
-        return -1;
-    }
-
-    free(data);
     return 0;
 }
